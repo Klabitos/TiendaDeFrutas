bd8a7ea3b49e65eb6a4d2fd7406e212a4b2b300c
Klabitos! <klabitos@usal.es>
2021-10-11 14:04:51 +0200

    [MOD] pulido y comprobado. LISTO PARA TAGGEAR

--- js/scriptFrutas.js
+++ js/scriptFrutas.js
@@ -3,7 +3,7 @@ var cestaKiwi=["kiwi", parseFloat(document.getElementById("precioKiwi").innerHTM
 var cestaPera=["pera", parseFloat(document.getElementById("precioPera").innerHTML), 0];
 var cestaManzana=["manzana", parseFloat(document.getElementById("precioManzana").innerHTML), 0];
 var cestaPlatano=["platano", parseFloat(document.getElementById("precioPlatano").innerHTML), 0];
-var cestaMandarina=["manarina", parseFloat(document.getElementById("precioMandarina").innerHTML), 0];
+var cestaMandarina=["mandarina", parseFloat(document.getElementById("precioMandarina").innerHTML), 0];
 var cestaNaranja=["naranja", parseFloat(document.getElementById("precioNaranja").innerHTML), 0];
 var cestaMelon=["melon", parseFloat(document.getElementById("precioMelon").innerHTML), 0];
 var cestaSandia=["sandia", parseFloat(document.getElementById("precioSandia").innerHTML), 0];
@@ -23,29 +23,42 @@ var limpiarCestaCompra = () => {
 
 
 //Funciones para tratar el array de cestaCompra
-var eliminarFrutasCeroKilos = (num) => num==0?false:true;
- // // //Para saber si tiene que ecribir kilos en singular o plurar
-var kilo_or_kilos = num => num<=1?"kilo":"kilos"
+var masDeCeroKilosFruta = (num) => num==0?false:true;
+var mostrarKilosFruta = (texAreaElement, i) => texAreaElement.value+=cestaCompra[i][0]+"  --- "+cestaCompra[i][2]+" "+kilo_or_kilos(cestaCompra[i][2])+"\n";
+function ordenarCestaCompra(){
+    CestaCompraOrdenadaAlfabeticamente =cestaCompra.sort(function(a,b){
+        return a[0]<b[0];
+    });
+    return CestaCompraOrdenadaAlfabeticamente;
+} 
+// // //Para saber si tiene que ecribir kilos en singular o plurar
+var kilo_or_kilos = num => num<=1?"kilo":"kilos";
 
 
 function mostrarCompra(){
-    limpiarTextArea();
     let texAreaElement = document.getElementById("textAreaFactura");
     let precioTotal = 0;
     let kilos = 0;
     let precioMedioKilo = 0;
+
+    limpiarTextArea();
+    cestaCompra=ordenarCestaCompra();
     texAreaElement.value+="\n";
+
     for(let i=0; i<cestaCompra.length; i++){
-        if (eliminarFrutasCeroKilos(cestaCompra[i][2])){texAreaElement.value+=cestaCompra[i][0]+"  --- "+cestaCompra[i][2]+" "+kilo_or_kilos(cestaCompra[i][2])+"\n"};
+        if (masDeCeroKilosFruta(cestaCompra[i][2])){
+            mostrarKilosFruta(texAreaElement, i);
+        }   
         precioTotal+=cestaCompra[i][2]*cestaCompra[i][1];
         kilos+=cestaCompra[i][2];
     }
     precioMedioKilo = precioTotal/kilos;
-    texAreaElement.value+="Precio total: "+precioTotal+" €"+"\n";
+    texAreaElement.value+="Precio total: "+precioTotal.toFixed(2)+" €"+"\n";
     texAreaElement.value+="Precio medio: "+precioMedioKilo.toFixed(2)+" €"+"\n";
     limpiarCestaCompra();
 }
 
+//Onload
 function re_start(){
     limpiarTextArea();
     limpiarCestaCompra();
