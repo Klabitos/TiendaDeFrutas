983a57e97575beb28b3bd0d7c71810d6acb5416d
Klabitos! <klabitos@usal.es>
2021-11-03 17:58:26 +0100

    [ADD] creacion de clases y objetos

--- js/scriptFrutas.js
+++ js/scriptFrutas.js
@@ -1,21 +1,19 @@
 /*TODO 
-CREAR RAMA DE DESARROLLO
+
 OBJETOS(DATOS KILOS PRECIO) INSTANCIA DE UNA CLASE
-- CLASE FRUTA: NOMBRE+PRECIO+KILOS
--FRUTAS VERANO: PROXIMIDAD+REGION
--FRUTAS INVIERNO: CONSERVAR FUERA DE LA NEVERA
+
 TRATAMIENTO DE ERRORES
 ORGANIZAR CODIGO EN ARCHIVOS
 ----
 FUNCIONALIDAD-->
-CAJA DE TEXTO PARA METER KILOS DE FRUTA QUE QUEREMOS
+
 PULSAR LA IMAGEN INTRODUCIRÁ LOS KILOS
 --AL PULSAR EN FRUTA Y SOLO SI HAY MÁS DE 0 KILOS SE AÑADE LA FRUTA Y SUS KILOS , CON FONDO DE ALGÚN COLOR
 --CADA FRUTA VA AÑADIENDO INFORMACIÓN
 --SE SUBRAYA CADA VEZ QUE SE HA AÑADIDO ESA FRUTA Y SDE QUITA EL SOMBREADO DE L AULTIMA AÑADIDA
 --TODO SE QUITA CUANDO SE ACABA EL PEDIDO
 
---CAJA DE TEXTO VACIO AL FINAL + BOTON PARA FINALIZAR
+
 --BOTON METE EN LA CAJA DE TEXTO LA COMPRA
 ----1 LINEA CON FECHA Y HORA DE LA FINALIZACIÓN DEL PEDIDO
 ----CADA TIPO DE FRUTA APARECERÁ SOLO 1 VEZ CON TODOS LOS KILOS
@@ -42,18 +40,19 @@ Precio medio: xx €/kg
 //Variables globales. Almacenan nombre+precio+kilos
 "use strict";
 
-
+/////////////////////////////////////////////////////////////////////////////////////////////
 var arrayDivs = document.getElementsByClassName("frame");
 var inputs = document.getElementsByClassName("inputTextKG");
 var textoCambiante = document.getElementsByClassName("invisiblePrice_moved");
 var arraySpan = document.getElementsByTagName("span");
 var arrayAuxiliar=[false,false,false,false,false,false,false,false,false,false] //Evitar un miniBug que hacia que se pudiera cambiar el TextoCambiante al hacer click
+//Sin este array con cada cambio en el input nos cambiaria el texto, así sabemos si ya lo ha cambiado o si no lo ha cambiado aun
 
 for(let i=0; i<10; i++){
   inputs[i].addEventListener("input", () =>{
     if(arrayAuxiliar[i]==false && inputs[i].value!=""){ 
       arrayAuxiliar[i]=true; 
-      fadein(parseFloat(inputs[i].placeholder).toFixed(2), i);   
+      fadein(parseFloat(inputs[i].placeholder).toFixed(2), i);   //Tiene que cambiar el mensaje de la caja con un FadeIn, le paso el mensaje (que es el precio y viene en el placeholder) y el iterador(i)
     }
   }); 
   inputs[i].addEventListener("blur", ()=>{
@@ -64,7 +63,6 @@ for(let i=0; i<10; i++){
     }
   );
 }
-
 
 function fadein(precio, i){
   textoCambiante[i].classList.add('hide');
@@ -79,102 +77,72 @@ function fadein(precio, i){
   }, 260); 
   setTimeout(function() { 
     textoCambiante[i].classList.remove('hide')
-}, 260);
+  }, 260);
 }
 
+function re_start() {
+  //limpiarAreaDerechaInmediatamente
+  limpiarTextArea();
+  limpiarCestaCompra();
+}
 
+class Fruit{
+  constructor(nombre, precio, kilosVez, kilosTotales){
+    this.nombre = nombre;
+    this.precio = precio;
+    this.kilosVez = kilosVez;
+    this.kilosTotales = kilosTotales;
+  }
+}
+class WinterFruit extends Fruit{
+  constructor(nombre, precio, kilosVez, kilosTotales, conservarNevera){
+    super(nombre, precio, kilosVez, kilosTotales);
+    this.conservarNevera = conservarNevera;
+    
+  }
+}
+class SummerFruit extends Fruit{
+  constructor(nombre, precio, kilosVez, kilosTotales, proximidad, region){ //Son o no son de proximidad //Region=lugar
+    super(nombre, precio, kilosVez, kilosTotales);
+    this.proximidad = proximidad;
+    this.region = region;
+  }
+}
 
-var cestaKiwi = [
-  "kiwi",
-  parseFloat(document.getElementById("precioKiwi").placeholder),
-  0,
-];
-var cestaPera = [
-  "pera",
-  parseFloat(document.getElementById("precioPera").innerHTML),
-  0,
-];
-var cestaManzana = [
-  "manzana",
-  parseFloat(document.getElementById("precioManzana").innerHTML),
-  0,
-];
-var cestaPlatano = [
-  "platano",
-  parseFloat(document.getElementById("precioPlatano").innerHTML),
-  0,
-];
-var cestaMandarina = [
-  "mandarina",
-  parseFloat(document.getElementById("precioMandarina").innerHTML),
-  0,
-];
-var cestaNaranja = [
-  "naranja",
-  parseFloat(document.getElementById("precioNaranja").innerHTML),
-  0,
-];
-var cestaMelon = [
-  "melon",
-  parseFloat(document.getElementById("precioMelon").innerHTML),
-  0,
-];
-var cestaSandia = [
-  "sandia",
-  parseFloat(document.getElementById("precioSandia").innerHTML),
-  0,
-];
-var cestaPinna = [
-  "piña",
-  parseFloat(document.getElementById("precioPinna").innerHTML),
-  0,
-];
-var cestaLimon = [
-  "limon",
-  parseFloat(document.getElementById("precioLimon").innerHTML),
-  0,
-];
+var kiwi = new WinterFruit("Kiwi",parseFloat(document.getElementById("precioKiwi").placeholder),0,0,false);
+var pera = new WinterFruit("Pera",parseFloat(document.getElementById("precioPera").placeholder),0,0,false);
+var manzana = new WinterFruit("Manzana",parseFloat(document.getElementById("precioManzana").placeholder),0,0,false);
+var platano = new WinterFruit("Plátano",parseFloat(document.getElementById("precioPlatano").placeholder),0,0,false);
+var mandarina = new SummerFruit("Mandarina",parseFloat(document.getElementById("precioMandarina").placeholder),0,0,false,"Salamanca");
+var naranja = new SummerFruit("Naranja",parseFloat(document.getElementById("precioNaranja").placeholder),0,0,false,"Palencia");
+var melon = new SummerFruit("Melón",parseFloat(document.getElementById("precioMelon").placeholder),0,0,false,"Huelva");
+var sandia = new SummerFruit("Sandía",parseFloat(document.getElementById("precioSandia").placeholder),0,0,false,"Canarias");
+var pinna = new SummerFruit("Piña",parseFloat(document.getElementById("precioPinna").placeholder),0,0,false,"Macondo");
+var limon = new WinterFruit("Limón",parseFloat(document.getElementById("precioLimon").placeholder),0,0,true);
+
 //Almacena todos los productos
-var cestaCompra = [
-  cestaKiwi,
-  cestaPera,
-  cestaManzana,
-  cestaPlatano,
-  cestaMandarina,
-  cestaNaranja,
-  cestaMelon,
-  cestaSandia,
-  cestaPinna,
-  cestaLimon,
-];
+var cestaCompra = [kiwi, pera, manzana, platano, mandarina, naranja, melon, sandia, pinna, limon];
 
 //Funcionalidad básica de modificar y preparar la web
 var addUnKiloAFrutaPorID = (ID_FrutaPosicionCestaCompra) => {
   cestaCompra[ID_FrutaPosicionCestaCompra][2]++;
   limpiarTextArea();
 };
+
+////////////////////////////////////////////////////////////
+//      Limpieza de variables y cuadros de texto          //
+////////////////////////////////////////////////////////////
 var limpiarTextArea = () =>
   (document.getElementById("textAreaFactura").value = "");
 var limpiarCestaCompra = () => {
-  cestaCompra = [
-    cestaKiwi,
-    cestaPera,
-    cestaManzana,
-    cestaPlatano,
-    cestaMandarina,
-    cestaNaranja,
-    cestaMelon,
-    cestaSandia,
-    cestaPinna,
-    cestaLimon,
-  ];
-  cestaCompra.map((cesta) => (cesta[2] = 0));
+  cestaCompra = [kiwi, pera, manzana, platano, mandarina, naranja, melon, sandia, pinna, limon]; //Volvemos a ordenar
+  cestaCompra.map((cesta) => { //Vaciamos los kilos totales y relativos
+    (cesta[2] = 0);
+    (cesta[3] = 0) 
+  });
 };
 //Onload
-function re_start() {
-  limpiarTextArea();
-  limpiarCestaCompra();
-}
+
 //Funciones para tratar el array de cestaCompra
 function ordenarCestaCompra() {
   let cestaCompraOrdenadaAlfabeticamente = cestaCompra.sort(function (a, b) {
